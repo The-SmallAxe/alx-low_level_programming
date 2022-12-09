de465bc8b9bff1601fd805c05d959b47b9904caf
The-SmallAxe <kevinodhiambo665@gmail.com>
2022-12-09 14:56:09 -0800

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns base 16 numbers in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0'; 
+	char letter = 'a';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return ('0');
+}
