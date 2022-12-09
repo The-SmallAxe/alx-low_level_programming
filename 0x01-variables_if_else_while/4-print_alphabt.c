576ce0caebf7c10d983303b014b143b5a893db4d
The-SmallAxe <kevinodhiambo665@gmail.com>
2022-12-09 14:06:26 -0800

    Print all except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabets apart from e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
