2ea27047442f2b8d3258cd52e199aec3f08a676f
The-SmallAxe <kevinodhiambo665@gmail.com>
2022-12-09 13:37:45 -0800

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	int ch = 'a';
-		while (ch <= 'z');
-		{
-			putchar (ch);
-			ch++;
-		}
+	while (ch <= 'z');
+	{
+		putchar (ch);
+		ch++;
+	}
 	putchar ('\n');
 	return (0);
 }
