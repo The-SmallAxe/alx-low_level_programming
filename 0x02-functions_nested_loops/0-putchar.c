d7a37524acac8dee98b73bad10e65ffc3d5a3b43
The-SmallAxe <kevinodhiambo665@gmail.com>
2022-12-14 07:54:11 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints _putchar. followed by newline
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str [] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+	return (0);
+}
