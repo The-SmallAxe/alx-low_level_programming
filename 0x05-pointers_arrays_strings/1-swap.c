bffc8b615bc132d164a5c73c9664b90a82106cbd
The-SmallAxe <kevinodhiambo665@gmail.com>
2022-12-19 11:36:10 -0800

    Swapped Values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,14 +3,14 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @a - first pointer
- * @b - second pointer
+ * @a: first pointer
+ * @b: second pointer
+ * int c: address where swapped value will be sent to
  * Return: Always 0 (Success)
  */
 
 void swap_int(int *a, int *b)
 {
-	
 	int c;
 
 	c = *a;
