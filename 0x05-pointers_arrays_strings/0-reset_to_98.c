2f7a56d1d833b771b15e27452e54c52a4f2ad17a
The-SmallAxe <kevinodhiambo665@gmail.com>
2022-12-19 09:35:35 -0800

    reset and update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * main - function takes pointer to int and updates value to 98
+ * reset_to_98 - function takes pointer to int and updates value to 98
  * @n: integer pointer
  * Return: Always 0 (Success)
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
